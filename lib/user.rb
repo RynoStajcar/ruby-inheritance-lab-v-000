68ff0ee032b4b6c0adfc5cdc9b7ab36cf56b0dd6
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-25 21:29:18 +0000

    automatically backed up by learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,20 +1,4 @@
 class User
   attr_accessor :first_name, :last_name
 
-  def first_name=(first)
-    @first_name = first
-  end
-
-  def first_name
-    @first_name
-  end
-
-  def last_name=(last)
-    @last_name = last
-  end
-
-  def last_name
-    @last_name
-  end
-
 end
